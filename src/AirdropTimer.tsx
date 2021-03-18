b3ebb3945093d4f78620ea9ea7cf209e84c6fd21
심철환 <a@akaiv.com>
2021-03-18 20:02:06 +0900

    Add ANC airdrop timer

--- src/AirdropTimer.tsx
+++ src/AirdropTimer.tsx
@@ -1,41 +1,84 @@
 import { format, formatDistanceToNow } from "date-fns"
 import numeral from "numeral"
+import { GENESIS_ANC, GENESIS_MIR } from "./helpers/calc"
 import { getNextAirdropDate, getNextAirdropHeight } from "./helpers/calc"
 import styles from "./AirdropTimer.module.scss"
 
 const FMT = "MMM d (E) aa HH:mm"
+const IMAGE_MIR = "https://whitelist.mirror.finance/images/MIR.png"
+const IMAGE_ANC = "https://whitelist.anchorprotocol.com/logo/ANC.png"
+
+interface Contents {
+  title: string
+  content: string
+}
 
 const AirdopTimer = ({ height, now }: { height: number; now: Date }) => {
-  const nextAidropHeight = getNextAirdropHeight(height)
-  const nextAirdropDate = getNextAirdropDate(height, now)
+  const nextAidropHeightMIR = getNextAirdropHeight(GENESIS_MIR, height)
+  const nextAirdropDateMIR = getNextAirdropDate(GENESIS_MIR, height, now)
+  const nextAidropHeightANC = getNextAirdropHeight(GENESIS_ANC, height)
+  const nextAirdropDateANC = getNextAirdropDate(GENESIS_ANC, height, now)
 
   const contents = [
     {
       title: "Latest block",
       content: numeral(height).format(),
     },
+  ]
+
+  const contentsMIR = [
     {
       title: "Next airdrop block",
-      content: numeral(nextAidropHeight).format(),
+      content: numeral(nextAidropHeightMIR).format(),
     },
     {
       title: "Next airdrop date (estimated)",
-      content: format(nextAirdropDate, FMT),
-    },
-    {
-      title: "Next airdrop to now (estimated)",
-      content: formatDistanceToNow(nextAirdropDate, { addSuffix: true }),
+      content: format(nextAirdropDateMIR, FMT),
     },
   ]
 
+  const contentsANC = [
+    {
+      title: "Next airdrop block",
+      content: numeral(nextAidropHeightANC).format(),
+    },
+    {
+      title: "Next airdrop date (estimated)",
+      content: format(nextAirdropDateANC, FMT),
+    },
+  ]
+
+  const toNowMIR = formatDistanceToNow(nextAirdropDateMIR, { addSuffix: true })
+  const toNowANC = formatDistanceToNow(nextAirdropDateANC, { addSuffix: true })
+
+  const renderContents = ({ title, content }: Contents) => (
+    <article className={styles.article} key={title}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.content}>{content}</p>
+    </article>
+  )
+
   return (
     <>
-      {contents.map(({ title, content }) => (
-        <article className={styles.article} key={title}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.content}>{content}</p>
-        </article>
-      ))}
+      <section className={styles.latest}>
+        {contents.map(renderContents)}
+      </section>
+
+      <section className={styles.card}>
+        <h1 className={styles.heading}>
+          <img src={IMAGE_MIR} width={30} height={30} alt="MIR" />
+          {toNowMIR}
+        </h1>
+        {contentsMIR.map(renderContents)}
+      </section>
+
+      <section className={styles.card}>
+        <h1 className={styles.heading}>
+          <img src={IMAGE_ANC} width={30} height={30} alt="ANC" />
+          {toNowANC}
+        </h1>
+        {contentsANC.map(renderContents)}
+      </section>
     </>
   )
 }
--- src/helpers/calc.ts
+++ src/helpers/calc.ts
@@ -1,16 +1,21 @@
 import { addSeconds } from "date-fns"
 
 const BLOCK_TIME = 6.47 // seconds
-const GENESIS = 920000
+export const GENESIS_MIR = 920000
+export const GENESIS_ANC = 2179600
 const INTERVAL = 100000
 
-export const getNextAirdropHeight = (height: number) => {
-  const rest = GENESIS % INTERVAL
+export const getNextAirdropHeight = (genesis: number, height: number) => {
+  const rest = genesis % INTERVAL
   return Math.ceil((height - rest) / INTERVAL) * INTERVAL + rest
 }
 
-export const getNextAirdropDate = (height: number, now: Date) => {
-  const nextHeight = getNextAirdropHeight(height)
+export const getNextAirdropDate = (
+  genesis: number,
+  height: number,
+  now: Date
+) => {
+  const nextHeight = getNextAirdropHeight(genesis, height)
   const distance = (nextHeight - height) * BLOCK_TIME
   const nextDate = addSeconds(now, distance)
   return nextDate
